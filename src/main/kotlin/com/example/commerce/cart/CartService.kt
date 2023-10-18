ff679eedb3121c7718ae1ad39c123e348c2346f5
LeeKiara <hbin1028@gmail.com>
2023-10-19 02:30:24 +0900

    장바구니 담기 수정

--- src/main/kotlin/com/example/commerce/cart/CartService.kt
+++ src/main/kotlin/com/example/commerce/cart/CartService.kt
@@ -3,7 +3,7 @@ package com.example.commerce.cart
 import com.example.commerce.auth.AuthProfile
 import org.jetbrains.exposed.sql.Database
 import org.jetbrains.exposed.sql.insert
-import org.jetbrains.exposed.sql.insertAndGetId
+import org.jetbrains.exposed.sql.select
 import org.jetbrains.exposed.sql.transactions.transaction
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
@@ -20,15 +20,29 @@ class CartService(private val database: Database) {
         println("\n<<< CartService createCart>>>")
         println(" -- 입력값 확인 :  " + authProfile.id)
 
+        var insertCartId = 0L;
+        val existCartId = transaction {
+            Cart.slice(Cart.id)
+                    .select { Cart.profileId eq authProfile.id }
+                    .singleOrNull()
+        }
+
         val cartId = transaction {
             try {
-                // 장바구니 등록
-                val insertCartId = Cart.insert {
-                    it[this.profileId] = authProfile.id;
-                    it[this.createDate] = LocalDateTime.now()
-                } get Cart.id // Explicitly specify the Cart.id column
 
-                println(" -- 장바구니 등록 insertCartId : " + insertCartId.toString())
+                if (existCartId == null) {
+                    // 장바구니 등록
+                    insertCartId = Cart.insert {
+                        it[this.profileId] = authProfile.id;
+                        it[this.createDate] = LocalDateTime.now()
+                    } get Cart.id // Explicitly specify the Cart.id column
+
+                    println(" -- 장바구니 등록 insertCartId : " + insertCartId.toString())
+                } else {
+                    insertCartId = existCartId[Cart.id];
+
+                    println(" -- 장바구니 이미 존재 : " + insertCartId.toString())
+                }
 
                 // 장바구니 item 등록
                 val cartItemId = CartItem.insert {
