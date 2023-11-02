e42cf70b5ae0c8055ac4402eb99923d2749177a0
LeeHyunMee <hbin1028@gmail.com>
2023-11-02 18:48:12 +0900

    event book 조회 수정

--- src/main/kotlin/com/example/commerce/publisher/EventBookController.kt
+++ src/main/kotlin/com/example/commerce/publisher/EventBookController.kt
@@ -1,5 +1,8 @@
 package com.example.commerce.publisher
 
+import com.example.commerce.books.Books
+import org.jetbrains.exposed.sql.JoinType
+import org.jetbrains.exposed.sql.select
 import org.jetbrains.exposed.sql.selectAll
 import org.jetbrains.exposed.sql.transactions.transaction
 import org.springframework.web.bind.annotation.GetMapping
@@ -15,20 +18,41 @@ class EventBookController {
 
         val e = EventBooks
 
-        e.selectAll().map { r ->
-            EventBookResponse(
-                    r[e.id].value,
-                    r[e.itemId],
-                    r[e.title],
-                    r[e.description],
-                    r[e.cover],
-                    r[e.textSentence],
-                    r[e.mentSentence],
-                    r[e.authorImage],
-                    r[e.author],
-                    r[e.publisher],
-                    r[e.authorDescription],
-            )
-        }
+        (EventBooks)
+                .join(Books, JoinType.INNER, onColumn = Books.itemId, otherColumn = EventBooks.itemId)
+                .slice(Books.id, e.itemId, e.title, e.description, e.cover, e.textSentence,
+                        e.mentSentence, e.authorImage, e.author, e.publisher, e.authorDescription)
+                .select { e.title neq "" }
+                .map { r ->
+                    EventBookResponse(
+                            r[Books.id].value,
+                            r[e.itemId],
+                            r[e.title],
+                            r[e.description],
+                            r[e.cover],
+                            r[e.textSentence],
+                            r[e.mentSentence],
+                            r[e.authorImage],
+                            r[e.author],
+                            r[e.publisher],
+                            r[e.authorDescription],
+                    )
+                }
+
+//        e.selectAll().map { r ->
+//            EventBookResponse(
+//                    r[e.id].value,
+//                    r[e.itemId],
+//                    r[e.title],
+//                    r[e.description],
+//                    r[e.cover],
+//                    r[e.textSentence],
+//                    r[e.mentSentence],
+//                    r[e.authorImage],
+//                    r[e.author],
+//                    r[e.publisher],
+//                    r[e.authorDescription],
+//            )
+//        }
     }
 }
