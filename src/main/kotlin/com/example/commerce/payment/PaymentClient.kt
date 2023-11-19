ac051508d2c5ae1381e9b47b7a8ca120ada850a6
LeeKiara <hbin1028@gmail.com>
2023-11-19 23:15:47 +0900

    rabbit 주소 변경

--- src/main/kotlin/com/example/commerce/payment/PaymentClient.kt
+++ src/main/kotlin/com/example/commerce/payment/PaymentClient.kt
@@ -4,8 +4,8 @@ import org.springframework.cloud.openfeign.FeignClient
 import org.springframework.web.bind.annotation.GetMapping
 
 
-@FeignClient(name = "paymentClient", url = "http://192.168.0.5:8082/payment")
-//@FeignClient(name = "paymentClient", url = "http://192.168.100.204:8082/payment")
+//@FeignClient(name = "paymentClient", url = "http://192.168.0.5:8082/payment")
+@FeignClient(name = "paymentClient", url = "http://192.168.100.204:8082/payment")
 interface PaymentClient {
     @GetMapping("/bank-deposit")
     fun getBankDeposit(): List<BankDepositResponse>
