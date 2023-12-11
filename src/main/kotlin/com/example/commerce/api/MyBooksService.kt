c4772affc8f6c2b8008108508eb45fc07ed7fa10
Aejin Kang <dowlsdl003@naver.com>
2023-12-11 15:56:50 +0900

    레디스 시간 설정 수정

--- src/main/kotlin/com/example/commerce/api/MyBooksService.kt
+++ src/main/kotlin/com/example/commerce/api/MyBooksService.kt
@@ -27,7 +27,7 @@ class MyBooksService(
 
 //    @Scheduled(cron = "0 20 14 * * *")
 //@Scheduled(cron = "0 */30 * * * *")
-    @Scheduled(cron = "0 35 15 ? * MON", zone = "UTC+9")
+    @Scheduled(fixedRate = 1000 * 60 * 60 * 24 * 7)
     fun scheduledFetchBooksData() {
         println("--- newBookData fetching ---")
         val items = myBooksClient.newBooksFetch()
@@ -58,7 +58,7 @@ class MyBooksService(
 
 //    @Scheduled(cron = "0 20 14 * * *")
 //    @Scheduled(cron = "0 */30 * * * *")
-    @Scheduled(cron = "0 35 15 ? * MON", zone = "UTC+9")
+    @Scheduled(fixedRate = 1000 * 60 * 60 * 24 * 7)
     fun scheduledFetchBestBooksData() {
         println("--- bestData fetching ---")
         val items = myBooksClient.bestFetch()
